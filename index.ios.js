2c2ba00f4e12c90ee029c1b11258df8e43f444db
Andrzej Porebski <fkuciapa@yahoo.com>
2015-10-30 23:45:18 +0100

    Sample app using the plugin.

---
+++ index.ios.js
@@ -0,0 +1,266 @@
+/**
+ * Sample React Native App with SQLite
+ * Demo the react-native-sqlite-storage
+ * 
+ *
+ */
+'use strict';
+
+var React = require('react-native');
+var SQLite = require('react-native-sqlite-storage');
+
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  ListView,
+} = React;
+
+var database_name = "Test.db";
+var database_version = "1.0";
+var database_displayname = "SQLite Test Database";
+var database_size = 200000;
+var db;
+
+var SQLiteDemo = React.createClass({
+    getInitialState(){
+        return {
+            progress: [],
+            dataSource: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 !== row2,
+            })
+        };
+    },
+
+    componentWillUnmount(){
+        console.log("Closing database ...");
+        if (db) {
+            db.close(this.closeCB, this.errorCB);
+        }
+    },
+
+    errorCB(err) {
+        this.state.progress.push("SQL Error: " + err);
+        this.setState(this.state);
+    },
+
+    successCB() {
+    },
+
+    openCB() {
+        this.state.progress.push("Database OPEN");
+        this.setState(this.state);
+    },
+
+    closeCB() {
+        console.log("Database CLOSED.");
+    },
+
+    deleteCB() {
+        console.log("Database DELETED");
+        this.state.progress.push("Database DELETED");
+        this.setState(this.state);
+    },
+
+    populateDatabase(db){
+        var that = this;
+        that.state.progress.push("Database integrity check");
+        that.setState(that.state);
+        db.executeSql('SELECT 1 FROM Version LIMIT 1', [],
+            function () {
+                that.state.progress.push("Database is ready ... executing query ...");
+                that.setState(that.state);
+                db.transaction(that.queryEmployees);
+            },
+            function (error) {
+                console.log("received error:", error)
+                that.state.progress.push("Database not yet ready ... populating data");
+                that.setState(that.state);
+                db.transaction(that.populateDB, that.errorCB, function () {
+                    that.state.progress.push("Database populated ... executing query ...");
+                    that.setState(that.state);
+                    db.transaction(that.queryEmployees);
+                });
+            })
+    },
+
+    populateDB(tx) {
+        this.state.progress.push("Executing DROP stmts");
+        this.setState(this.state);
+
+        tx.executeSql('DROP TABLE IF EXISTS Employees;');
+        tx.executeSql('DROP TABLE IF EXISTS Offices;');
+        tx.executeSql('DROP TABLE IF EXISTS Departments;');
+
+        this.state.progress.push("Executing CREATE stmts");
+        this.setState(this.state);
+
+        tx.executeSql('CREATE TABLE IF NOT EXISTS Version( '
+            + 'version_id INTEGER PRIMARY KEY NOT NULL); ', [], this.successCB, this.errorCB);
+
+        tx.executeSql('CREATE TABLE IF NOT EXISTS Departments( '
+            + 'department_id INTEGER PRIMARY KEY NOT NULL, '
+            + 'name VARCHAR(30) ); ', [], this.successCB, this.errorCB);
+
+        tx.executeSql('CREATE TABLE IF NOT EXISTS Offices( '
+            + 'office_id INTEGER PRIMARY KEY NOT NULL, '
+            + 'name VARCHAR(20), '
+            + 'longtitude FLOAT, '
+            + 'latitude FLOAT ) ; ', [], this.successCB, this.errorCB);
+
+        tx.executeSql('CREATE TABLE IF NOT EXISTS Employees( '
+            + 'employe_id INTEGER PRIMARY KEY NOT NULL, '
+            + 'name VARCHAR(55), '
+            + 'office INTEGER, '
+            + 'department INTEGER, '
+            + 'FOREIGN KEY ( office ) REFERENCES Offices ( office_id ) '
+            + 'FOREIGN KEY ( department ) REFERENCES Departments ( department_id ));', [], this.successCB, this.errorCB);
+
+        this.state.progress.push("Executing INSERT stmts");
+        this.setState(this.state);
+
+
+        tx.executeSql('INSERT INTO Departments (name) VALUES ("Client Services");', [], this.successCB, this.errorCB);
+        tx.executeSql('INSERT INTO Departments (name) VALUES ("Investor Services");', [], this.successCB, this.errorCB);
+        tx.executeSql('INSERT INTO Departments (name) VALUES ("Shipping");', [], this.successCB, this.errorCB);
+        tx.executeSql('INSERT INTO Departments (name) VALUES ("Direct Sales");', [], this.successCB, this.errorCB);
+
+        tx.executeSql('INSERT INTO Offices (name, longtitude, latitude) VALUES ("Denver", 59.8,  34.);', [], this.successCB, this.errorCB);
+        tx.executeSql('INSERT INTO Offices (name, longtitude, latitude) VALUES ("Warsaw", 15.7, 54.);', [], this.successCB, this.errorCB);
+        tx.executeSql('INSERT INTO Offices (name, longtitude, latitude) VALUES ("Berlin", 35.3, 12.);', [], this.successCB, this.errorCB);
+        tx.executeSql('INSERT INTO Offices (name, longtitude, latitude) VALUES ("Paris", 10.7, 14.);', [], this.successCB, this.errorCB);
+
+        tx.executeSql('INSERT INTO Employees (name, office, department) VALUES ("Sylvester Stallone", 2,  4);', [], this.successCB, this.errorCB);
+        tx.executeSql('INSERT INTO Employees (name, office, department) VALUES ("Elvis Presley", 2, 4);', [], this.successCB, this.errorCB);
+        tx.executeSql('INSERT INTO Employees (name, office, department) VALUES ("Leslie Nelson", 3,  2);', [], this.successCB, this.errorCB);
+        tx.executeSql('INSERT INTO Employees (name, office, department) VALUES ("Fidel Castro", 3, 1);', [], this.successCB, this.errorCB);
+        tx.executeSql('INSERT INTO Employees (name, office, department) VALUES ("Bill Clinton", 1, 3);', [], this.successCB, this.errorCB);
+        tx.executeSql('INSERT INTO Employees (name, office, department) VALUES ("Margaret Thatcher", 1, 2);', [], this.successCB, this.errorCB);
+        tx.executeSql('INSERT INTO Employees (name, office, department) VALUES ("Donald Trump", 1, 3);', [], this.successCB, this.errorCB);
+        tx.executeSql('INSERT INTO Employees (name, office, department) VALUES ("Dr DRE", 2, 2);', [], this.successCB, this.errorCB);
+        tx.executeSql('INSERT INTO Employees (name, office, department) VALUES ("Samantha Fox", 2, 1);', [], this.successCB, this.errorCB);
+
+    },
+
+    queryEmployees(tx) {
+        tx.executeSql('SELECT a.name, b.name as deptName FROM Employees a, Departments b WHERE a.department = b.department_id', [], this.queryEmployeesSuccess);
+    },
+
+    queryEmployeesSuccess(tx, results) {
+        this.state.progress.push("Query completed");
+        this.setState(this.state);
+        var len = results.rows.length;
+        for (let i = 0; i < len; i++) {
+            let row = results.rows.item(i);
+            this.state.progress.push(`Dept Id: ${row.name}, Dept Name: ${row.deptName}`);
+        }
+        ;
+        this.setState(this.state);
+    },
+
+    loadAndQueryDB(){
+        this.state.progress.push("Opening database ...");
+        this.setState(this.state);
+        db = SQLite.openDatabase(database_name, database_version, database_displayname, database_size, this.openCB, this.errorCB);
+        this.populateDatabase(db);
+    },
+
+    deleteDatabase(){
+        this.state.progress = ["Deleting database"];
+        this.setState(this.state);
+        SQLite.deleteDatabase(database_name, this.deleteCB, this.errorCB);
+    },
+
+    runDemo(){
+        this.state.progress = ["Starting SQLite Demo"];
+        this.setState(this.state);
+        this.loadAndQueryDB();
+    },
+
+    renderProgressEntry(entry){
+        return (<View style={listStyles.li}>
+            <View>
+                <Text style={listStyles.title}>{entry}</Text>
+            </View>
+        </View>)
+    },
+
+    render(){
+        var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        return (<View style={styles.mainContainer}>
+            <View style={styles.toolbar}>
+                <Text style={styles.toolbarButton} onPress={this.runDemo}>
+                    Run Demo!
+                </Text>
+                <Text style={styles.toolbarButton} onPress={this.deleteDatabase}>
+                    Delete DB!
+                </Text>
+            </View>
+            <ListView
+                dataSource={ds.cloneWithRows(this.state.progress)}
+                renderRow={this.renderProgressEntry}
+                style={listStyles.liContainer}/>
+        </View>);
+    }
+});
+
+var listStyles = StyleSheet.create({
+    li: {
+        borderBottomColor: '#c8c7cc',
+        borderBottomWidth: 0.5,
+        paddingTop: 15,
+        paddingRight: 15,
+        paddingBottom: 15,
+    },
+    liContainer: {
+        backgroundColor: '#fff',
+        flex: 1,
+        paddingLeft: 15,
+    },
+    liIndent: {
+        flex: 1,
+    },
+    liText: {
+        color: '#333',
+        fontSize: 17,
+        fontWeight: '400',
+        marginBottom: -3.5,
+        marginTop: -3.5,
+    },
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  toolbar: {
+      backgroundColor: '#51c04d',
+      paddingTop: 30,
+      paddingBottom: 10,
+      flexDirection: 'row'
+  },
+  toolbarButton: {
+      color: 'blue',
+      textAlign: 'center',
+      flex: 1
+  },
+  mainContainer: {
+      flex: 1
+  }
+});
+
+AppRegistry.registerComponent('SQLiteDemo', () => SQLiteDemo);
