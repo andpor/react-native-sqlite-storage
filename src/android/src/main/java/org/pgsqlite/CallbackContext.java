137a0083e47718ddaff1a5f0c9f6714e769a651c
Andrzej Porebski <fkuciapa@yahoo.com>
2015-11-15 17:55:22 +0100

    Correct tab stops.

--- src/android/src/main/java/org/pgsqlite/CallbackContext.java
+++ src/android/src/main/java/org/pgsqlite/CallbackContext.java
@@ -15,80 +15,81 @@ import org.json.JSONObject;
 
 public class CallbackContext {
 
-	private static final String LOG_TAG = "SQLitePlugin";
+    private static final String LOG_TAG = "SQLitePlugin";
 
-	private Callback successCallback;
-	private Callback errorCallback;
+    private Callback successCallback;
+    private Callback errorCallback;
 
-	public CallbackContext(Callback success, Callback error) {
-		this.successCallback = success;
-		this.errorCallback = error;
-	}
+    public CallbackContext(Callback success, Callback error) {
+        this.successCallback = success;
+        this.errorCallback = error;
+    }
 
-	/**
-	 * Helper for success callbacks that just returns the Status.OK by default
-	 *
-	 * @param message The message to add to the success result.
-	 */
-	public void success(JSONObject message) {
-		try {
-			WritableMap writableMap = SQLitePluginConverter.jsonToReact(message);
-			successCallback.invoke(writableMap);
-		} catch (JSONException ex){
-			errorCallback.invoke("Internal error converting results:"+ex.getMessage());
-		}
-	}
-	/**
-	* Helper for success callbacks that just returns the Status.OK by default
-	*
-	* @param message The message to add to the success result.
-	*/
-	public void success(String message) {
-		successCallback.invoke(message);
-	}
+    /**
+     * Helper for success callbacks that just returns the Status.OK by default
+     *
+     * @param message The message to add to the success result.
+     */
+    public void success(JSONObject message) {
+        try {
+            WritableMap writableMap = SQLitePluginConverter.jsonToReact(message);
+            successCallback.invoke(writableMap);
+        } catch (JSONException ex){
+            errorCallback.invoke("Internal error converting results:"+ex.getMessage());
+        }
+    }
 
-	/**
-	 * Helper for success callbacks that just returns the Status.OK by default
-	 *
-	 * @param message The message to add to the success result.
-	 */
-	public void success(JSONArray message) {
-		try {
-			WritableArray writableArray = SQLitePluginConverter.jsonToReact(message);
-			successCallback.invoke(writableArray);
-		} catch (JSONException ex){
-			errorCallback.invoke("Internal error converting results:"+ex.getMessage());
-		}
+    /**
+     * Helper for success callbacks that just returns the Status.OK by default
+     *
+     * @param message The message to add to the success result.
+     */
+    public void success(String message) {
+        successCallback.invoke(message);
+    }
 
-	}
+    /**
+     * Helper for success callbacks that just returns the Status.OK by default
+     *
+     * @param message The message to add to the success result.
+     */
+    public void success(JSONArray message) {
+        try {
+            WritableArray writableArray = SQLitePluginConverter.jsonToReact(message);
+            successCallback.invoke(writableArray);
+        } catch (JSONException ex){
+            errorCallback.invoke("Internal error converting results:"+ex.getMessage());
+        }
 
-	/**
-	 * Helper for success callbacks that just returns the Status.OK by default
-	 */
-	public void success() {
-		successCallback.invoke("Success");
-	}
+    }
 
-	/**
-	 * Helper for error callbacks that just returns the Status.ERROR by default
-	 *
-	 * @param message The message to add to the error result.
-	 */
-	public void error(JSONObject message) {
+    /**
+     * Helper for success callbacks that just returns the Status.OK by default
+     */
+    public void success() {
+        successCallback.invoke("Success");
+    }
+
+    /**
+     * Helper for error callbacks that just returns the Status.ERROR by default
+     *
+     * @param message The message to add to the error result.
+     */
+    public void error(JSONObject message) {
         try {
             WritableMap writableMap = SQLitePluginConverter.jsonToReact(message);
             errorCallback.invoke(writableMap);
         } catch (JSONException ex){
             errorCallback.invoke("Internal error converting results:"+ex.getMessage());
         }
-	}
+    }
 
     /**
-	 * Helper for error callbacks that just returns the Status.ERROR by default
-	 *
-	 * @param message The message to add to the error result.
-	 */
-	public void error(String message) {
-		errorCallback.invoke(message);
-	}
+     * Helper for error callbacks that just returns the Status.ERROR by default
+     *
+     * @param message The message to add to the error result.
+     */
+    public void error(String message) {
+        errorCallback.invoke(message);
+    }
 }
