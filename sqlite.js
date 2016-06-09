fc33d771936d859064d7f5b6d9713023e0cb4a16
Andrzej Porebski <fkuciapa@yahoo.com>
2016-06-10 00:06:52 +0200

    Update copyright statement.

--- sqlite.js
+++ sqlite.js
@@ -2,7 +2,7 @@
  * sqlite.js
  *
  * Created by Andrzej Porebski on 10/29/15.
- * Copyright (c) 2015 Andrzej Porebski.
+ * Copyright (c) 2015-16 Andrzej Porebski.
  *
  * This library is available under the terms of the MIT License (2008).
  * See http://opensource.org/licenses/alphabetical for full text.
