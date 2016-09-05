28386728c0d2774c1d3d6319044518bdf6f5cc8c
Hagen Hübel <hhuebel@itinance.com>
2016-09-06 00:01:59 +0200

    add example for ATTACH DATABASE

---
+++ examples/attach-database/src/test.js
@@ -0,0 +1,42 @@
+
+var SQLite = require('react-native-sqlite-storage')
+
+export function doTest() {
+  const dbSecond = SQLite.openDatabase({name: 'second'}, openCB, errorCB);
+
+  const dbMaster = SQLite.openDatabase({
+    name: 'master',
+    attachDatabase: 'second', attachAlias: 'second'
+  }, openCB, errorCB);
+
+  dbMaster.executeSql('CREATE TABLE IF NOT EXISTS bar (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, data TEXT)');
+  dbMaster.executeSql("INSERT INTO bar (data) VALUES ('bar-test')")
+
+  dbSecond.executeSql('CREATE TABLE IF NOT EXISTS foo (id INTEGER, data TEXT)');
+  dbSecond.executeSql("INSERT INTO foo (data) VALUES ('test-foo')")
+
+
+  dbMaster.readTransaction( (tx) => {
+    var sql = "SELECT data FROM bar LIMIT 1";
+    tx.executeSql(sql, [], (tx, results) => {
+        console.log( results.rows.item(0).data );
+    });
+  })
+
+  dbMaster.readTransaction( (tx) => {
+    var sql = "SELECT data FROM second.foo LIMIT 1";
+    tx.executeSql(sql, [], (tx, results) => {
+        console.log( results.rows.item(0).data );
+    });
+  })
+
+}
+
+
+function openCB() {
+
+}
+
+function errorCB() {
+
+}
