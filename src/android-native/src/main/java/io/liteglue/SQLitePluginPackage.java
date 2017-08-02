d44e9e4521453bfd7768909211092fe57c0afef7
Radek Czemerys <radko93@gmail.com>
2017-08-02 22:03:09 +0200

    Remove createJSModules @ovveride marker - RN 0.47 compatibility (#188)
    * Remove depreciated createJSModules @ovveride marker
    
    * Remove depreciated createJSModules @ovveride marker

--- src/android-native/src/main/java/io/liteglue/SQLitePluginPackage.java
+++ src/android-native/src/main/java/io/liteglue/SQLitePluginPackage.java
@@ -38,7 +38,7 @@ public class SQLitePluginPackage implements ReactPackage {
       return modules;
     }
 
-    @Override
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
