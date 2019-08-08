892ddb948064f17a9afe4ad3e3f4315d58533d55
Carlos Roberto <carlos.parreira@sige.pro.br>
2019-08-08 11:52:46 -0300

    start enable autolink

--- react-native.config.js
+++ react-native.config.js
@@ -4,7 +4,10 @@ module.exports = {
       project: 'src/ios/SQLite.xcodeproj'
     },
     android: {
-      sourceDir: 'src/android'
+      sourceDir: 'src/android',
+      settingsGradlePath: 'src/android/settings.gradle',
+      buildGradlePath: 'src/android/build.gradle',
+      manifestPath: 'src/android/src/main/AndroidManifest.xml'
     }
   }
 }
