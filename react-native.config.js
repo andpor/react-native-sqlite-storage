e680b0020b53f072f7993699b49582c9014e8a2f
Carlos Roberto <carlos.parreira@sige.pro.br>
2019-08-08 14:22:27 -0300

    remove configs

--- react-native.config.js
+++ react-native.config.js
@@ -2,12 +2,7 @@ module.exports = {
   project: {
     ios: {
       project: 'src/ios/SQLite.xcodeproj'
-    },
-    android: {
-      sourceDir: 'src/android',
-      settingsGradlePath: 'src/android/settings.gradle',
-      buildGradlePath: 'src/android/build.gradle',
-      manifestPath: 'src/android/src/main/AndroidManifest.xml'
     }
+    android: {}
   }
 }
