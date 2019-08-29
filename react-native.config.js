baef09df196777cba351ccb3e9f65b03b7848b40
John Ruble <john.ruble@atomicobject.com>
2019-08-29 09:23:54 -0400

    access android-native behind USE_ANDROID_SQLITE_NATIVE env var

--- react-native.config.js
+++ react-native.config.js
@@ -5,7 +5,7 @@ module.exports = {
 				project: './platforms/ios/SQLite.xcodeproj'
 			},
 			android: {
-				sourceDir: './platforms/android'
+				sourceDir: `./platforms/android${process.env.USE_ANDROID_SQLITE_NATIVE ? "-native" : ""}`
 			}
 		}
 	}
