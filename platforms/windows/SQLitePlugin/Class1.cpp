e0c8a04843ce9d8b4ef7217d8ff77cea64ae079d
Nik Tsytsarkin <nikt@microsoft.com>
2020-10-27 14:05:30 -0700

    New Project Setup

---
+++ platforms/windows/SQLitePlugin/Class1.cpp
@@ -0,0 +1,9 @@
+﻿#include "pch.h"
+#include "Class1.h"
+
+using namespace SQLitePlugin;
+using namespace Platform;
+
+Class1::Class1()
+{
+}
---
+++ platforms/windows/SQLitePlugin/Class1.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+namespace SQLitePlugin
+{
+    public ref class Class1 sealed
+    {
+    public:
+        Class1();
+    };
+}
