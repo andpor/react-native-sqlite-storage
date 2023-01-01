6ddf0d1909a9cd02e0fb12c62a71ef3034ab7a4c
mackenzie <mackenzie@yourmoms>
2023-01-01 14:11:40 -0600

    Fix `createFromLocation` for Windows

--- platforms/windows/SQLitePlugin/SQLitePlugin.cpp
+++ platforms/windows/SQLitePlugin/SQLitePlugin.cpp
@@ -307,7 +307,7 @@ namespace SQLitePlugin
                     {
                         try
                         {
-                            CopyDbAsync(assetFile, to_hstring(*dbFileName)).GetResults();
+                            CopyDbAsync(assetFile, to_hstring(*dbFileName));
                         }
                         catch (hresult_error const& ex)
                         {
